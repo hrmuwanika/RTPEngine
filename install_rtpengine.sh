6713141477a64432395a9d3eea08766886b067b7
Henry Robert Muwanika <hrmuwanika@gmail.com>
2020-07-07 04:30:27 +0200

    Create install_rtpengine.sh

---
+++ install_rtpengine.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+#--------------------------------------------
+# Installation of RTPengine on debian 10 
+#--------------------------------------------
+
+#----------------------------------------------------
+# Disable password authentication
+#----------------------------------------------------
+sudo sed -i 's/#ChallengeResponseAuthentication yes/ChallengeResponseAuthentication no/' /etc/ssh/sshd_config
+sudo sed -i 's/UsePAM yes/UsePAM no/' /etc/ssh/sshd_config 
+sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
+sudo service sshd restart
+
+#--------------------------------------------------
+# Update Server
+#--------------------------------------------------
+echo -e "\n============= Update Server ================"
+sudo apt update
+sudo apt upgrade -y
+sudo apt autoremove -y
+
+sudo apt install -y vim
+
+# disable vim visual mode
+echo "set mouse-=a" >> ~/.vimrc
+
+#-----------------------------------------------
+# Installation of FFMPEG v4
+#----------------------------------------------
+# Install FFmpeg 4 using External Repository
+sudo apt install -y gnupg gnupg1 gnupg2
+add-apt-repository ppa:jonathonf/ffmpeg-4
+sudo apt update
+
+sudo apt install -y ffmpeg
+ffmpeg -version
+
+#--------------------------------------------
+# Install required libraries
+#--------------------------------------------
+sudo apt install -y debhelper default-libmysqlclient-dev gperf iptables-dev libavcodec-dev libavfilter-dev libcurl4-openssl-dev \
+libavformat-dev libavutil-dev libbencode-perl libcrypt-openssl-rsa-perl libcrypt-rijndael-perl libdigest-crc-perl \
+libdigest-hmac-perl libevent-dev libhiredis-dev libio-multiplex-perl libio-socket-inet6-perl libiptc-dev libjson-glib-dev \
+libnet-interface-perl libpcap0.8-dev libsocket6-perl libspandsp-dev libswresample-dev libsystemd-dev libxmlrpc-core-c3-dev \
+markdown dkms module-assistant keyutils libnfsidmap2 nfs-common rpcbind libconfig-tiny-perl dh-autoreconf git unzip \
+libpcre3 libpcre3-dev build-essential
+
+#--------------------------------------------
+# Download rtpengine from source
+#--------------------------------------------
+cd /usr/src
+git clone https://github.com/sipwise/rtpengine.git
+cd rtpengine
+
+echo "########## G729 Installation ################"
+
+VER=1.0.4
+curl https://codeload.github.com/BelledonneCommunications/bcg729/tar.gz/$VER >bcg729_$VER.orig.tar.gz
+tar zxf bcg729_$VER.orig.tar.gz 
+cd bcg729-$VER 
+git clone https://github.com/ossobv/bcg729-deb.git debian 
+dpkg-buildpackage -us -uc -sa
+cd ..
+dpkg -i libbcg729-*.deb
+
+########################################################
+
+# Now let’s check the RTPengine dependencies again:
+dpkg-checkbuilddeps
+
+# If you get an empty output you’re good to start building the packages:
+dpkg-buildpackage 
+
+cd ..
+
+dpkg -i ngcp-rtpengine-daemon_*.deb
+
+# Getting it Running:
+cp /etc/rtpengine/rtpengine.sample.conf  /etc/rtpengine/rtpengine.conf
+
+# Uncomment and update the interface IP of your server
+vim /etc/rtpengine/rtpengine.conf
+
+# Edit ngcp-rtpengine-daemon and ngcp-rtpengine-recording-daemon files:
+vim /etc/default/ngcp-rtpengine-daemon
+        RUN_RTPENGINE=yes
+
+dpkg -i ngcp-rtpengine-iptables_*.deb
+dpkg -i ngcp-rtpengine-kernel-dkms_*.deb
+dpkg -i ngcp-rtpengine-kernel-source_*.deb
+dpkg -i ngcp-rtpengine-recording-daemon_*.deb
+
+vim /etc/default/ngcp-rtpengine-recording-daemon
+        RUN_RTPENGINE_RECORDING=yes
+
+cp /etc/rtpengine/rtpengine-recording.sample.conf /etc/rtpengine/rtpengine-recording.conf
+vim /etc/rtpengine/rtpengine-recording.conf
+mkdir /var/spool/rtpengine
+
+dpkg -i ngcp-rtpengine-utils_*.deb
+dpkg -i ngcp-rtpengine_*.deb
+
+systemctl enable ngcp-rtpengine-daemon.service 
+systemctl enable ngcp-rtpengine-recording-daemon.service 
+systemctl enable ngcp-rtpengine-recording-nfs-mount.service
+
+systemctl restart ngcp-rtpengine-daemon.service 
+systemctl restart ngcp-rtpengine-recording-daemon.service 
+systemctl restart ngcp-rtpengine-recording-nfs-mount.service
+
+ps -ef | grep rtpengine
